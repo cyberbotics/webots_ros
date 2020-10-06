158e7edc354849d15a0c3804ef61e09dc8d3dddc
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2020-10-06 15:51:52 +0200

    Fixed case sensitivity of 'no_gui' argument

--- scripts/webots_launcher.py
+++ scripts/webots_launcher.py
@@ -30,7 +30,7 @@ options, args = optParser.parse_args()
 if 'WEBOTS_HOME' not in os.environ:
     sys.exit('WEBOTS_HOME environment variable not defined.')
 command = [os.path.join(os.environ['WEBOTS_HOME'], 'webots'), '--mode=' + options.mode, options.world]
-if options.noGui == 'true':
+if options.noGui.lower() == 'true':
     command.append('--stdout')
     command.append('--stderr')
     command.append('--batch')
