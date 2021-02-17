90593eeac011f990dae715e4f4641636c0a21bde
Darko Lukic <lukicdarkoo@gmail.com>
2021-02-17 11:34:27 +0100

    Revert physics reset

--- src/complete_test.cpp
+++ src/complete_test.cpp
@@ -112,7 +112,6 @@
 #include <webots_ros/node_get_velocity.h>
 #include <webots_ros/node_remove.h>
 #include <webots_ros/node_reset_functions.h>
-#include <webots_ros/node_reset_physics.h>
 #include <webots_ros/node_set_string.h>
 #include <webots_ros/node_set_velocity.h>
 #include <webots_ros/node_set_visibility.h>
@@ -3073,11 +3072,10 @@ int main(int argc, char **argv) {
   // test reset_physics
   // if the node isn't a top Solid webots will throw a warning but still return true to ros
   ros::ServiceClient supervisor_node_reset_physics_client =
-    n.serviceClient<webots_ros::node_reset_physics>(model_name + "/supervisor/node/reset_physics");
-  webots_ros::node_reset_physics supervisor_node_reset_physics_srv;
+    n.serviceClient<webots_ros::node_reset_functions>(model_name + "/supervisor/node/reset_physics");
+  webots_ros::node_reset_functions supervisor_node_reset_physics_srv;
 
   supervisor_node_reset_physics_srv.request.node = from_def_node;
-  supervisor_node_reset_physics_srv.request.recursive = true;
   if (supervisor_node_reset_physics_client.call(supervisor_node_reset_physics_srv) &&
       supervisor_node_reset_physics_srv.response.success == 1)
     ROS_INFO("Node physics has been reset successfully.");
