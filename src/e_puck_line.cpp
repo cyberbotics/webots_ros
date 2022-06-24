4959572a2486f96d17bdeba27f321ae9c06dcfbc
Yannick Goumaz <yannick.goumaz@epfl.ch>
2022-06-24 09:30:22 +0200

    Update clang format

--- src/e_puck_line.cpp
+++ src/e_puck_line.cpp
@@ -612,7 +612,8 @@ int main(int argc, char **argv) {
 
     if (oam_active) {
       ROS_WARN("The robot has bumped into an obstacle. Please complete the code in the \"LineLeavingModule()\" function of "
-               "\"%s\" or simply remove the obstacle to get a correct behavior.", __FILE__);
+               "\"%s\" or simply remove the obstacle to get a correct behavior.",
+               __FILE__);
       wheelSrv.request.value = 0;
       leftWheelVelocityClient.call(wheelSrv);
       wheelSrv.request.value = 0;
